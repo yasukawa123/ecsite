f6d29dc5fbb5eeca072591d91e32c8f225b38b34
yasukawa <kazu050107@gmail.com>
2022-02-20 03:09:06 +0900

    table create

--- database/migrations/2022_02_14_215514_create_cart_items_table.php
+++ database/migrations/2022_02_14_215514_create_cart_items_table.php
@@ -14,8 +14,9 @@ class CreateCartItemsTable extends Migration
     public function up()
     {
         Schema::create('cart_items', function (Blueprint $table) {
-            $table->id();
             $table->timestamps();
+            $table->id();
+            
         });
     }
 
